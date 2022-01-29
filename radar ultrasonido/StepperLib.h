eee4789c55a616c76faf7759d627a8fb5716e4cd
Juan Chavez <98656519+brekill11@users.noreply.github.com>
2022-01-29 14:21:40 -0500

    Update StepperLib.h

--- radar ultrasonido/StepperLib.h
+++ radar ultrasonido/StepperLib.h
@@ -2,8 +2,9 @@
  * Stepper.h
  *
  * Created: 2019-06-19 10:10:24 a.m.
- *  Author: BladimirBaccaCortes
+ *  Author: Chavez, Montenegro
  */ 
 
 int isButtonPressed(unsigned char bitPos);
-void driveStepper();
+void driveStepperOclock(float *angulo,float anguloMotor);
+void driveStepperAnticlock(float *angulo, float anguloMotor);
